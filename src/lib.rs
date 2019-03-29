10482bb7e927652f2eef676891298033736ac56b
Dan Wadeson <dan.wadeson@tryonsolutions.com>
2019-03-29 11:20:42 -0500

    rectangles and lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+
+#[derive(PartialEq, Debug)]
+pub enum GraphicsPrimitive {
+    Rectangle { data: RectangleData },
+    Line { data: LineData },
+}
+
+#[derive(PartialEq, Debug)]
+pub struct RectangleData {
+    pub left: u32,
+    pub top: u32,
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct LineData {
+    pub x1: u32,
+    pub y1: u32,
+    pub x2: u32,
+    pub y2: u32,
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
