f686295ef4f830a87d984d9c3bfa5ad343214faf
Dan Wadeson <dan.wadeson@tryonsolutions.com>
2019-04-02 10:44:49 -0500

    added more primitives and added html canvas and drawing logic.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,141 @@
+use std::f64;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+pub struct CanvasPainter {
+    context: web_sys::CanvasRenderingContext2d
+}
+
+impl CanvasPainter {
+    pub fn new() -> CanvasPainter {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document.get_element_by_id("rainbow-unicorn-canvas").unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap();
+
+        let context = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+
+        CanvasPainter {
+            context: context
+        }
+    }
+
+    pub fn paint(&self, graphics_primitives: Vec<GraphicsPrimitive>) {
+        for gp in graphics_primitives.iter() {
+            match gp {
+                GraphicsPrimitive::Rectangle { data } => {
+                    // need to handle rounded corners
+                    self.context.rect(
+                        data.left as f64,
+                        data.top as f64,
+                        data.width as f64,
+                        data.height as f64
+                    );
+                    self.context.stroke();
+                }
+                GraphicsPrimitive::Line { data } => {
+                    self.context.begin_path();
+                    self.context.move_to(data.x1 as f64, data.y1 as f64);
+                    self.context.line_to(data.x2 as f64, data.y2 as f64);
+                    self.context.stroke();
+                }
+                GraphicsPrimitive::Ellipse { data } => {
+                    self.context.begin_path();
+                    self.context.ellipse(
+                        data.cx as f64,
+                        data.cy as f64,
+                        data.rx as f64,
+                        data.ry as f64,
+                        0.0,
+                        0.0,
+                        2.0 * f64::consts::PI
+                    );
+                }
+                GraphicsPrimitive::Text { data } => {
+                    // TODO - this
+                    // self.context.font = data.font;
+                    // self.context.fill_text(
+                    //     data.text,
+                    //     data.x as f64,
+                    //     data.y as f64
+                    // );
+                }
+                GraphicsPrimitive::Polygon { points } => {
+                    self.context.begin_path();
+                    self.context.move_to(points[0].x as f64, points[0].y as f64);
+                    
+                    for p in points.iter() {
+                        self.context.line_to(p.x as f64, p.y as f64);
+                    }
+
+                    // need to switch on this later based on specified styles
+                    self.context.stroke();
+
+                    self.context.close_path();
+                }
+                GraphicsPrimitive::RuPath { data } => {
+                    self.context.begin_path();
+                    self.context.move_to(data.start_x as f64, data.start_y as f64);
+
+                    for segment in data.segments.iter() {
+                        self.draw_path_segment(&self.context, segment);
+                    }
+
+                    self.context.stroke();
+                    self.context.close_path();
+                }
+            }
+        }
+    }
+
+    fn draw_path_segment(&self, context: &web_sys::CanvasRenderingContext2d, segment: &PathSegment) {
+        match segment {
+            PathSegment::MoveTo { to_x, to_y } => {
+                context.move_to(*to_x as f64, *to_y as f64);
+            }
+            PathSegment::LineTo { to_x, to_y } => {
+                context.line_to(*to_x as f64, *to_y as f64);
+            }
+            PathSegment::BezierCurveTo {
+                cp1_x,
+                cp1_y,
+                cp2_x,
+                cp2_y,
+                to_x,
+                to_y
+            } => {
+                context.bezier_curve_to(
+                    *cp1_x as f64,
+                    *cp1_y as f64,
+                    *cp2_x as f64,
+                    *cp2_y as f64,
+                    *to_x as f64,
+                    *to_y as f64
+                );
+            }
+            PathSegment::QuadraticCurveTo {
+                cp_x,
+                cp_y,
+                to_x,
+                to_y
+            } => {
+                context.quadratic_curve_to(
+                    *cp_x as f64,
+                    *cp_y as f64,
+                    *to_x as f64,
+                    *to_y as f64
+                );
+            }
+        }
+    }
+}
 
 #[derive(PartialEq, Debug)]
 pub enum GraphicsPrimitive {
@@ -5,7 +143,6 @@ pub enum GraphicsPrimitive {
     Line { data: LineData },
     Ellipse { data: EllipseData },
     Text { data: TextData },
-    Polyline { points: Vec<Point> },
     Polygon { points: Vec<Point> },
     RuPath { data: PathData }
 }
@@ -109,15 +246,6 @@ mod tests {
             text: "Hello".to_string(),
             font: "Dialog".to_string()
         }};
-    }
-
-    #[test]
-    fn make_polyline() {
-        GraphicsPrimitive::Polyline { points: vec![
-            Point { x: 1, y: 11 },
-            Point { x: 2, y: 21 },
-            Point { x: 3, y: 31 }
-        ]};
     }
 
     #[test]
